895423ca2d637c4c052fcdf1eba0a7bd7eb5aa50
vvinayakasai <vinayaksai12@gmail.com>
2022-11-19 17:40:11 +0530

    json normalize first commit

---
+++ gittest.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+json = [
+    {
+        'class': 'Year 1',
+        'student count': 20,
+        'room': 'Yellow',
+        'info': {
+            'teachers': {
+                'math': 'Rick Scott',
+                'physics': 'Elon Mask'
+            }
+        },
+        'students': [
+            {
+                'name': 'Tom',
+                'sex': 'M',
+                'grades': {'math': 66, 'physics': 77}
+            },
+            {
+                'name': 'James',
+                'sex': 'M',
+                'grades': {'math': 80, 'physics': 78}
+            },
+        ]
+    },
+    {
+        'class': 'Year 2',
+        'student count': 25,
+        'room': 'Blue',
+        'info': {
+            'teachers': {
+                'math': 'Alan Turing',
+                'physics': 'Albert Einstein'
+            }
+        },
+        'students': [
+            {'name': 'Tony', 'sex': 'M'},
+            {'name': 'Jacqueline', 'sex': 'F'},
+        ]
+    },
+]
+d = pd.json_normalize(json, record_path='students',
+                      meta=['class', 'student count', ['info', 'teachers', 'math'], ['info', 'teachers', 'physics']])
+with pd.option_context('display.max_columns', None):
+    print(d)
